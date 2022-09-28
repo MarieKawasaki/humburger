48b5ea1ff716c504c50e55f4697d4099db530eb6
MarieKawasaki <marie04103426@gmail.com>
2022-09-28 17:12:41 +0900

    フロントページcss編集

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+   <div id="content" class="narrowcolumn">
+     <h2 class="center">ごめんなさい、お探しのページはありませんでした＞＜</h2>
+     <p><a href="<?php echo home_url(); ?>">こちらから</a>トップページへお戻りください</p>
+	</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
--- footer.php
+++ footer.php
@@ -1,13 +1,7 @@
 <footer class="l-footer p-footer">
 
-            <ul class="p-footer__index">
-
-                <li class="p-footer__item c-index-item">
-                    <a href="#"><?php wp_nav_menu( array( 'theme_location' => 'footer_nav' ) ); ?></a>
-                </li>
-
-                
-            </ul>
+            
+            <?php wp_nav_menu( array( 'theme_location' => 'footer_nav' ) ); ?>
 
             <p class="p-footer__copyright c-copyright"><?php bloginfo( 'name' ); ?>2022</p>
         </footer>
